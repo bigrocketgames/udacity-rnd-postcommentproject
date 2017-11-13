2252a48c08b1ef9ff7b7bfed834357fed51911d0
Nathan Robbert <admin@bigrocketgames.com>
2017-11-12 20:11:05 -0800

    feat:  Adding react-router and connected the store to the application.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
-import App from './App';
+import App from './components/App';
+import store from './redux/store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
