5da1da7b07390b71e6927c583d21861ff421650f
Nathan Robbert <admin@bigrocketgames.com>
2017-11-27 19:23:36 -0800

    fix:  Now lets a user know if a post is no longer available.

--- src/views/singlePost/index.js
+++ src/views/singlePost/index.js
@@ -16,7 +16,9 @@ class SinglePost extends Component {
     super(props)
     this.state = {
       commentModalOpen: false,
-      comment: null
+      comment: null,
+      invalidPost: false,
+      postLoading: true
     }
 
     this.votePost = this.votePost.bind(this)
@@ -25,11 +27,28 @@ class SinglePost extends Component {
     this.deleteComment = this.deleteComment.bind(this)
     this.openCommentModal = this.openCommentModal.bind(this)
     this.closeCommentModal = this.closeCommentModal.bind(this)
+    this.postLoaded = this.postLoaded.bind(this)
   }
   
   componentDidMount() {
-    this.props.getSinglePost(this.props.match.params.post_id)
     this.props.getComments(this.props.match.params.post_id)
+    this.props.getSinglePost(this.props.match.params.post_id)
+      .then((action) => this.postLoaded(action.posts))
+  }
+
+  postLoaded = (post) => {
+    if (post.deleted === false) {
+      this.setState((state) => ({
+        ...state,
+        postLoading: false
+      }))
+    } else {
+      this.setState((state) => ({
+        ...state,
+        postLoading: false,
+        invalidPost: true
+      }))
+    }
   }
 
   openCommentModal = (comment) => {
@@ -70,39 +89,56 @@ class SinglePost extends Component {
     const { post, comments } = this.props
     const { commentModalOpen, comment } = this.state
 
-    return(
-      <div className="container">
-        <h2 className="post-title text-center">{post.title}</h2>
-        <p className="byLine text-center">by: {post.author}</p>
-        <hr />
-        <p className="post-body">{post.body}</p>
-        <hr />
-        <div>
-          <p className="comment-count">{post.commentCount} comments</p>
-          <p className="post-votes"><button className="btn btn-primary btn-circle" id={post.id} onClick={(e) => this.votePost(e, "upVote")}><ThumbsUpIcon size={20}/></button><button className="btn btn-primary btn-circle" id={post.id} onClick={(e) => this.votePost(e, "downVote")}><ThumbsDownIcon size={20}/></button>  {post.voteScore}</p>
-        </div>
-        <Link to={{
-          pathname: `/${post.category}/${post.id}/edit`,
-          state: { post: post}
-        }} 
-        className="post-edit-link btn btn-secondary">Edit Post</Link>
-        <button className="btn btn-danger delete-button" id={post.id} onClick={this.deletePost} >Delete Post</button>
-        <hr />
-        <br />
-        <CommentForm parent_id={post.id} />
-        <br />
-        <h4 className="comments-title text-center">COMMENTS</h4>
-        {comments.length > 0 && comments.map((comment) => <Comments key={comment.id} comment={comment} voteComment={this.voteComment} deleteComment={this.deleteComment} openModal={this.openCommentModal} />) }
+    // Returns something different if waiting for the post to load.
+    if (this.state.postLoading) {
+      return(
+        <h3 className="text-center"> The requested post is loading</h3>
+      )
+    } else {
 
-
-        <Modal
-          isOpen={commentModalOpen}
-          onRequestClose={this.closeCommentModal}
-        >
-          { commentModalOpen === false ? null : <CommentForm comment={comment} closeModal={this.closeCommentModal} /> }
-        </Modal>
-      </div>
-    )
+      // Shows either the post or a message saying the message is not available any longer.
+      if(this.state.invalidPost) {
+        return(
+          <div className="container">
+            <h3 className="text-center">The requested post is no longer available.  Please return to the <Link to="/">home page</Link> to choose a new post.</h3>
+          </div>
+        )
+      } else {
+        return(
+          <div className="container">
+            <h2 className="post-title text-center">{post.title}</h2>
+            <p className="byLine text-center">by: {post.author}</p>
+            <hr />
+            <p className="post-body">{post.body}</p>
+            <hr />
+            <div>
+              <p className="comment-count">{post.commentCount} comments</p>
+              <p className="post-votes"><button className="btn btn-primary btn-circle" id={post.id} onClick={(e) => this.votePost(e, "upVote")}><ThumbsUpIcon size={20}/></button><button className="btn btn-primary btn-circle" id={post.id} onClick={(e) => this.votePost(e, "downVote")}><ThumbsDownIcon size={20}/></button>  {post.voteScore}</p>
+            </div>
+            <Link to={{
+              pathname: `/${post.category}/${post.id}/edit`,
+              state: { post: post}
+            }} 
+            className="post-edit-link btn btn-secondary">Edit Post</Link>
+            <button className="btn btn-danger delete-button" id={post.id} onClick={this.deletePost} >Delete Post</button>
+            <hr />
+            <br />
+            <CommentForm parent_id={post.id} />
+            <br />
+            <h4 className="comments-title text-center">COMMENTS</h4>
+            {comments.length > 0 && comments.map((comment) => <Comments key={comment.id} comment={comment} voteComment={this.voteComment} deleteComment={this.deleteComment} openModal={this.openCommentModal} />) }
+    
+    
+            <Modal
+              isOpen={commentModalOpen}
+              onRequestClose={this.closeCommentModal}
+            >
+              { commentModalOpen === false ? null : <CommentForm comment={comment} closeModal={this.closeCommentModal} /> }
+            </Modal>
+          </div>
+        )
+      }
+    }
   }
 }
 
